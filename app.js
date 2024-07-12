2d56f8aba3f481521d80c6ef6139e176d1ff9ef9
Chelsiiy <151575298+Chelsiiy@users.noreply.github.com>
2024-07-12 13:47:34 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Homescreen from './Homescreen';
+import Cart from './Cart';
+import ProductDetail from './Productdetails'
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  const [cart, setCart] = useState([]);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home">
+          {props => <Homescreen {...props} cart={cart} setCart={setCart} />}
+        </Stack.Screen>
+        <Stack.Screen name="Cart">
+          {props => <Cart {...props} cart={cart} setCart={setCart} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
